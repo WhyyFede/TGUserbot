767096783b228fdd68f9f3280683e5830f0732ea
FedeBerti <43616163+FedeBerti@users.noreply.github.com>
2020-11-02 00:56:00 +0100

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,10 @@
+#Metti qua api di e api hash creabili da https://my.telegram.org/apps
+id = ""
+hash = ""
+
+#Inserisci i dati del gruppo di controllo
+chat = ""
+chatid = ""
+
+#Inserisci i messaggi da floddare, esempio msg = ["messaggio1", "messaggio2", "messaggio3"]
+msg = []
