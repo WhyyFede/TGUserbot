9ada66d2f75653faeab58c68c8034c3de4c05a3a
FedeBerti <43616163+FedeBerti@users.noreply.github.com>
2020-11-02 01:07:14 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
-#Metti qua api di e api hash creabili da https://my.telegram.org/apps
+#Metti qua api di e api hash creabili da https://my.telegram.org/apps. Es: id = "45678", hash = "abcdefghi789456"
 id = ""
 hash = ""
 
-#Inserisci i dati del gruppo di controllo, link del gruppo in "chat" e chatid del gruppo in "chatid"
+#Inserisci i dati del gruppo di controllo, link del gruppo in "chat" e chatid del gruppo in "chatid". Es: chat = "https://t.me/joinchat/abcdefghijk12345", chatid = -100123456789
 chat = ""
-chatid = ""
+chatid = 
 
-#Inserisci i messaggi da floddare, esempio msg = ["messaggio1", "messaggio2", "messaggio3"]
+#Inserisci i messaggi da floddare. Es: msg = ["messaggio1", "messaggio2", "messaggio3"]
 msg = []
